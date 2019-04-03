912c50b5e4562ce18962efaebfae0f66c93b14ab
positivepeng <903871772@qq.com>
2019-04-03 21:18:28 +0800

    origin version no opencv

---
+++ compareTwoCuttedCatFace.py
@@ -0,0 +1,40 @@
+from imgToVec import Img2Vec
+from PIL import Image
+from sklearn.metrics.pairwise import cosine_similarity
+
+img2vec = Img2Vec()
+
+def changePicDepth(img):
+    ###################################
+    #改变位宽度
+    if len(img.getbands()) > 3:
+        img.load() # required for png.split()
+        background= Image.new("RGB", img.size, (255, 255, 255))
+        background.paste(img, mask=img.split()[3]) # 3 is the alpha channel
+    else:
+        background = img
+    return background
+    ###################################
+
+def comparePicWithImg(img1,img2):
+    background1 = changePicDepth(img1)
+    background2 = changePicDepth(img2)
+    
+    vec1 = img2vec.get_vec(background1)
+    vec2 = img2vec.get_vec(background2)
+
+    return cosine_similarity(vec1.reshape((1, -1)), vec2.reshape((1, -1)))[0][0]
+
+def comparePicWithPath(filepath1,filepath2):
+    img1 = Image.open(filename1)
+    img2 = Image.open(filename2)
+
+    return comparePicWithImg(img1,img2)
+
+
+
+if __name__ == '__main__':    
+    filename1 = r"./images/1.jpg"
+    filename2 = r"./images/2.png" 
+
+    print(comparePicWithPath(filename1,filename2))
